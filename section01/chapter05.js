f960fa4a3454d109c2e3b16301f1dc0ec8c86917
jaemin0619 <wjdwoals000619@naver.com>
2026-09-07 19:32:20 +0900

    1.5) 자료형

---
+++ section01/chapter05.js
@@ -0,0 +1,18 @@
+// 1. Number Type
+
+let num1=27;
+let num2=1.5;
+let num3=-20;
+
+console.log(num1+num2);
+console.log(num1-num2);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1 % num2);
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan=NaN;
+
+console.log(1*"hello");
