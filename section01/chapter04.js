7d40ee2faf9ece688e3073e754425b361636d208
jaemin0619 <wjdwoals000619@naver.com>
2026-09-07 19:23:26 +0900

    1.4) 변수와 상수

--- section01/chapter04.js
+++ section01/chapter04.js
@@ -7,3 +7,18 @@ console.log(age);
 
 // 2. 상수 
 const birth = "2000.06.19"
+
+//3. 변수 명명규칙(네이밍 규칙)
+// 3-1. $, _ 제외한 기호는 사용할 수 없다. 
+
+// 3-2. 숫자로 시작할 수 없다. 
+let name1;
+let $name2;
+
+// 3-3. 예약어를 사용할 수 없다.
+//let if 
+
+// 4. 변수 명명 가이드
+let salesCount =1; 
+let refundCount=1;
+let totalSalesCount=salesCount-refundCount; 
